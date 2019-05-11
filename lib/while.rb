41082b2e84c19abae983e6ef2227cf1cc70ea3a7
Min Kang <kangm914@gmail.com>
2019-05-11 22:20:22 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,3 +1,10 @@
 count = 0 
 while count < 10 
 count += 1 
+
+describe "#using_while" do 
+	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do 
+	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	expect{ using_while }.to output(looping_string).to_stdout
+	end
+end
